4f4e40876b559df0005cdd737651819cda8eb2dc
mrmanwithoutaplan <bradley.ward608@gmail.com>
2025-12-04 14:17:32 +0000

    Fix actions being getting called twice by handling InputType

--- common/src/main/java/net/creeperhost/sharerecipe/ButtonInputHandler.java
+++ common/src/main/java/net/creeperhost/sharerecipe/ButtonInputHandler.java
@@ -2,6 +2,7 @@ package net.creeperhost.sharerecipe;
 
 import mezz.jei.common.input.IInternalKeyMappings;
 import mezz.jei.gui.input.IUserInputHandler;
+import mezz.jei.gui.input.InputType;
 import mezz.jei.gui.input.UserInput;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.Screen;
@@ -12,6 +13,10 @@ import java.util.Optional;
 public record ButtonInputHandler(Button button) implements IUserInputHandler {
     @Override
     public @NotNull Optional<IUserInputHandler> handleUserInput(Screen screen, UserInput input, IInternalKeyMappings keyBindings) {
+        InputType inputType = input.getInputType();
+        if(inputType == InputType.SIMULATE) {
+            return button.isMouseOver(input.getMouseX(), input.getMouseY()) ? Optional.of(this) : Optional.empty();
+        }
         boolean b = button.mouseClicked(input.getMouseX(), input.getMouseY(), input.getModifiers());
         if (b && button instanceof IconButton) {
             return Optional.of(this);
